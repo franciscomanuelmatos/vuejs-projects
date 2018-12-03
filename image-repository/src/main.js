bac8eee88647c5416cad16a5f7bedb0333c2a063
franciscomanuelmatos <franciscomanuelmatos@gmail.com>
2018-12-03 14:06:56 +0000

    Added router navigation guards

--- image-repository/src/main.js
+++ image-repository/src/main.js
@@ -1,24 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store';
 import VueRouter from 'vue-router';
 
+import store from './store';
 import AuthHandler from './components/AuthHandler';
 import ImageList from './components/ImageList';
 import UploadForm from './components/UploadForm';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
   routes: [
     { path: '/', component: ImageList },
-    { path: '/upload', component: UploadForm },
-    { path: '/oauth2/callback', component: AuthHandler }
+    { path: '/upload', component: UploadForm, beforeRouteEnter: checkAuth },
+    { path: '/oauth2/callback', component: AuthHandler, beforeRouteEnter: checkAuth }
   ],
-  mode: 'history'  
+  mode: 'history'
 });
+
+function checkAuth(to, from, next) {
+  if (!store.state.token) {
+    next('/')
+  } else {
+    next()
+  }
+}
 
 new Vue({
   render: h => h(App),
